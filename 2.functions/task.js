19e3701324c262b47f6219def97468959a3ac8b3
botass740 <127653295+botass740@users.noreply.github.com>
2024-02-08 11:33:52 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,9 +1,9 @@
-function getArrayParams(arr) {
-	let min, max, sum, avg;
-
-	min = Infinity;
-	max = -Infinity;
-	sum = 0;
+function getArrayParams(...arr) {
+	
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+	
 	for (let i = 0; i < arr.length; i += 1) {
 		if (arr[i] < min)
 			min = arr[i];
